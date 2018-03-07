58d8ba3b3cf86bf9b470a76ef125249dc296ffee
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-03-07 15:00:45 +0100

    add basic navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
 
+import {
+  Jumbotron,
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem
+} from 'react-bootstrap';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <header>
-          <h1 className="App-title">Tippspiel</h1>
-        </header>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">Tippspiel</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">
+              Tipps
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Gruppen
+            </NavItem>
+            <NavDropdown eventKey={3} title="Rangliste" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Spieler</MenuItem>
+              <MenuItem eventKey={3.2}>Gruppen</MenuItem>
+            </NavDropdown>
+            <NavItem eventKey={4} href="#">
+              Regeln
+            </NavItem>
+            <NavItem eventKey={5} href="#">
+              Admin
+            </NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">
+              Profil
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Ausloggen
+            </NavItem>
+          </Nav>
+        </Navbar>;
       </div>
     );
   }
