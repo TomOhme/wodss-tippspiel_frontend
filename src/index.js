f5f7ad173d23e730cb30f4e37e4b32448054bd95
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-03-10 19:02:18 +0100

    add language support with localize

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import { createStore } from 'redux'
 import App from './components/App'
 import betApp from './reducers';
 import './index.css';
+import { initialize } from 'react-localize-redux';
 
+{/* REDUX */}
 const store = createStore(
   betApp,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+{/* LANGUAGE SUPPORT */}
+const languages = [
+  { name: 'German', code: 'de' }
+]
+store.dispatch(initialize(languages));
 
 {/* make store available in all container components through Provider */ }
 render(
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
+import { localeReducer } from 'react-localize-redux';
 
 const betApp = combineReducers({
-    counter
+    counter,
+    locale: localeReducer
 })
 
 export default betApp;
