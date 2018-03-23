59279277b53d3f242dc7f18b929eae56f5be4cb4
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-03-23 07:48:33 +0100

    betgroups draft

--- src/components/BetGroups.js
+++ src/components/BetGroups.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import { localize } from 'react-localize-redux';
 
-const BetGroups = () => (
+import {
+    DropdownButton,
+    MenuItem,
+    Grid,
+    Row,
+    Col,
+    Button,
+    Table,
+    Glyphicon,
+} from 'react-bootstrap';
+
+const BetGroups = ({ translate }) => (
     <div>
-        BetGroups TODO
+        <DropdownButton title={translate('groups')}>
+            <MenuItem eventKey="1" active>Team T</MenuItem>
+            <MenuItem eventKey="2">Gruppe Kiosk</MenuItem>
+        </DropdownButton>
+
+        <Button bsStyle="primary">
+            {translate('joingroup')}
+        </Button>
+
+        <Button bsStyle="success">
+            {translate('creategroup')}
+        </Button>
+
+        <br />
+
+        <Table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>{translate('player')}</th>
+                    <th>{translate('points')}</th>
+                    <th>{translate('showbets')}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>hase</td>
+                    <td>666</td>
+                    <td><Button><Glyphicon glyph="user" /></Button></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>maulwurf</td>
+                    <td>555</td>
+                    <td><Button><Glyphicon glyph="user" /></Button></td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>zebra</td>
+                    <td>444</td>
+                    <td><Button><Glyphicon glyph="user" /></Button></td>
+                </tr>
+            </tbody>
+        </Table>
+
+        <Button bsStyle="danger">
+            {translate('leavegroup')}
+        </Button>
     </div>
 );
 
-export default BetGroups;
+export default localize(BetGroups, 'locale');
