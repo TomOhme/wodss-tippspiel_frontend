7ecc7c71e6c4593d7d0d0e38cf2178e4a9c30bb7
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-04-17 11:45:59 +0200

    add markdown, set rules page

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,32 +15,33 @@ const Header = ({ translate }) => (
     <PageHeader>
       {translate('title')}
     </PageHeader>
+
     <Navbar>
       <Nav>
-        <NavItem eventKey={1} href="/">
-          { translate('bets') }
-            </NavItem>
+        <NavItem eventKey={1} href="/" className="navitem-custom">
+          {translate('bets')}
+        </NavItem>
         <NavItem eventKey={2} href="/betgroups">
-          { translate('betgroups') }
-            </NavItem>
+          {translate('betgroups')}
+        </NavItem>
         <NavDropdown eventKey={3} title="Rangliste" id="basic-nav-dropdown">
           <MenuItem eventKey={3.1} href="/playerscoreboard">{translate('players')}</MenuItem>
           <MenuItem eventKey={3.2} href="/groupscoreboard">{translate('groups')}</MenuItem>
         </NavDropdown>
         <NavItem eventKey={4} href="/rules">
-          { translate('rules') }
-            </NavItem>
+          {translate('rules')}
+        </NavItem>
         <NavItem eventKey={5} href="/admin" hidden={false}>
-          { translate('admin') }
-            </NavItem>
+          {translate('admin')}
+        </NavItem>
       </Nav>
       <Nav pullRight>
         <NavItem eventKey={1} href="/profile">
-          { translate('profile') }
-            </NavItem>
+          {translate('profile')}
+        </NavItem>
         <NavItem eventKey={2} href="#" style={{ marginRight: 50 }}>
-          { translate('logout') }
-            </NavItem>
+          {translate('logout')}
+        </NavItem>
       </Nav>
     </Navbar>
   </div>
--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import { localize } from 'react-localize-redux';
 
-const Rules = () => (
-    <div>
-        Rules TODO
+const Rules = ({ translate }) => (
+    <div style={{maxWidth:600}}>
+        <ReactMarkdown source={translate('rule1')} />
+        <ReactMarkdown source={translate('rule2')} />
+        <ReactMarkdown source={translate('rule3')} />
+        <ReactMarkdown source={translate('rule4')} />
+        <ReactMarkdown source={translate('rule5')} />
+        <ReactMarkdown source={translate('rule6')} />
+        <ReactMarkdown source={translate('rule7')} />
+        <ReactMarkdown source={translate('rule8')} />
+        <ReactMarkdown source={translate('rule9')} />
     </div>
 );
 
-export default Rules;
+export default localize(Rules, 'locale');
