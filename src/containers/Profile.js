d63ba49515108a43a7259fbcac773447d77da1af
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-05-02 13:02:33 +0200

    Add accordion panel

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -4,20 +4,53 @@ import { getTranslate } from 'react-localize-redux';
 import { changeMailOnServer, changePasswordOnServer, deleteProfileOnServer } from '../actions';
 
 import {
-    Button
+    Button,
+    Panel,
+    PanelGroup
 } from 'react-bootstrap';
 
 import ChangeMailDialog from './ChangeMailDialog';
 
-let Profile = ({user, translate, dispatch, changeMailOnServer}) => (
+let Profile = ({ user, translate, dispatch, changeMailOnServer }) => (
     <div>
 
-        {/* Change email */}
-        <ChangeMailDialog translate={translate} changeMailOnServer={changeMailOnServer} />
+        <PanelGroup accordion id="profilePanel">
 
-        {/* Change Password */}
+            {/* Change email */}
+            <Panel eventKey="1">
+                <Panel.Heading>
+                    <Panel.Title toggle>
+                        {translate("changemail")}
+                    </Panel.Title>
+                </Panel.Heading>
+                <Panel.Body collapsible>
+                <ChangeMailDialog translate={translate} changeMailOnServer={changeMailOnServer} />
+                </Panel.Body>
+            </Panel>
 
-        {/* Delete profile */}
+            {/* Reset Password */}
+            <Panel eventKey="2">
+                <Panel.Heading>
+                    <Panel.Title toggle>
+                        {translate("resetpassword")}
+                    </Panel.Title>
+                </Panel.Heading>
+                <Panel.Body collapsible>
+                </Panel.Body>
+            </Panel>
+
+            {/* Delete profile */}
+            <Panel eventKey="3">
+                <Panel.Heading>
+                    <Panel.Title toggle>
+                        {translate("deleteprofile")}
+                    </Panel.Title>
+                </Panel.Heading>
+                <Panel.Body collapsible>
+                </Panel.Body>
+            </Panel>
+
+        </PanelGroup>
 
     </div>
 );
