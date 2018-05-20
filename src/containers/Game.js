3f7098bd91b8b5c72a8740eb418d6eb6e95469be
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-05-21 01:27:35 +0200

    Finals game names

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -18,6 +18,15 @@ import BetSaveButton from '../containers/BetSaveButton';
 let Game = ({ game, dispatch, translate, currentLanguage }) => (
 
     <div className="game">
+        {
+            (game.name !== undefined)
+                ?
+                <h3 className="game-name">
+                    {translate(game.name)}
+                </h3>
+                :
+                null
+        }
         <div className="border-gold">
             <Grid>
                 <Row>
@@ -31,78 +40,78 @@ let Game = ({ game, dispatch, translate, currentLanguage }) => (
                     <BetSaveButton game={game} />
                 </Row>
                 <Row>
-                <Col md={4} className="align">
-                    <span className="country">
-                        {translate(game.home.name)}
-                    </span>
-                    <Image src={require('../img/' + game.home.name + '.png')} />
-                </Col>
-
-                <Col md={3} className="score align">
-                    <Form inline>
-                        <FormControl
-                            value={game.home.bet}
-                            disabled={game.finished}
-                            onChange={(event) => dispatch(setScore(event, "home", game.id))}
-                            type="number"
-                            style={{ width: 60 }}
-                        >
-                        </FormControl>
-                        <span>
-                            :
+                    <Col md={4} className="align">
+                        <span className="country">
+                            {translate(game.home.name)}
                         </span>
-                        <FormControl
-                            value={game.guest.bet}
-                            disabled={game.finished}
-                            onChange={(event) => dispatch(setScore(event, "guest", game.id))}
-                            type="number"
-                            style={{ width: 60 }}
-                        >
-                        </FormControl>
-                    </Form>
-                </Col>
+                        <Image src={require('../img/' + game.home.name + '.png')} />
+                    </Col>
 
-                <Col md={4} className="align">
-                    <Image src={require('../img/' + game.guest.name + '.png')} />
-                    <span className="country">
-                        {translate(game.guest.name)}
-                    </span>
-                </Col>
+                    <Col md={3} className="score align">
+                        <Form inline>
+                            <FormControl
+                                value={game.home.bet}
+                                disabled={game.finished}
+                                onChange={(event) => dispatch(setScore(event, "home", game.id))}
+                                type="number"
+                                style={{ width: 60 }}
+                            >
+                            </FormControl>
+                            <span>
+                                :
+                        </span>
+                            <FormControl
+                                value={game.guest.bet}
+                                disabled={game.finished}
+                                onChange={(event) => dispatch(setScore(event, "guest", game.id))}
+                                type="number"
+                                style={{ width: 60 }}
+                            >
+                            </FormControl>
+                        </Form>
+                    </Col>
+
+                    <Col md={4} className="align">
+                        <Image src={require('../img/' + game.guest.name + '.png')} />
+                        <span className="country">
+                            {translate(game.guest.name)}
+                        </span>
+                    </Col>
                 </Row>
                 <Row>
-            <Label className="pull-left">
-                {game.date}
-            </Label>
-            <Label className="pull-left">
-                {game.time}
-            </Label>
-            <Label className="pull-right fix-pull">
-                {translate(game.place)}
-            </Label>
-            {
-                // only display results when game is finished
-                (game.finished === true) ?
-                    (
-                        <div className="finished-labels">
-                            <Label>
-                                {translate('winner')}: {translate(game.winner)}
-                            </Label>
-                            <Label>
-                                {translate('goalshome')}: {game.homegoals}
-                            </Label>
-                            <Label>
-                                {translate('goalsguest')}: {game.guestgoals}
-                            </Label>
-                            <Label>
-                                {translate('goaldifference')}: {game.difference}
-                            </Label>
-                            <Label>
-                                {translate('total')}: {game.total}
-                            </Label>
-                        </div>
-                    )
-                    : null
-            }
+                    <Label className="pull-left">
+                        {game.date}
+                    </Label>
+                    <Label className="pull-left">
+                        {game.time}
+                    </Label>
+                    <Label className="pull-right fix-pull">
+                        {translate(game.place)}
+                    </Label>
+                    {
+                        // only display results when game is finished
+                        (game.finished === true) ?
+                            (
+                                <div className="finished-labels">
+                                    <Label>
+                                        {translate('winner')}: {translate(game.winner)}
+                                    </Label>
+                                    <Label>
+                                        {translate('goalshome')}: {game.homegoals}
+                                    </Label>
+                                    <Label>
+                                        {translate('goalsguest')}: {game.guestgoals}
+                                    </Label>
+                                    <Label>
+                                        {translate('goaldifference')}: {game.difference}
+                                    </Label>
+                                    <Label>
+                                        {translate('total')}: {game.total}
+                                    </Label>
+                                </div>
+                            )
+                            : null
+                    }
                 </Row>
             </Grid>
         </div>
