2f5602accce280240675d36c454cf2d0c737b346
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-05-27 22:25:55 +0200

    Make ranks fair

--- src/reducers/playerScoreBoardReducer.js
+++ src/reducers/playerScoreBoardReducer.js
@@ -8,9 +8,21 @@ const playerScoreBoardReducer = (state = initialState, action) => {
     switch (action.type) {
         case "GETPLAYERRANKINGSUCCESS":
             var newPlayerRanking = action.playerRanking;
+
             _.each(newPlayerRanking, (player) => {
                 player.rank = _.indexOf(newPlayerRanking, player) + 1;
             });
+
+            // equalize rank when users have same scores
+            for (var i = 0; i < newPlayerRanking, i++;) {
+                var firstPlayer = newPlayerRanking[i]
+                var secondPlayer = newPlayerRanking[i+1]
+                if (secondPlayer != undefined 
+                    && firstPlayer.score === secondPlayer.score) {
+                    secondPlayer.rank = firstPlayer.rank;
+                }
+            }
+
             newState = newPlayerRanking;
             return newState;
         default:
